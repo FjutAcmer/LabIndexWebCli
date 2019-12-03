9b8a1e398aeb277cae949743f007432166243a7e
axiang <wyxworkmail@163.com>
2019-12-03 11:57:06 +0800

    Initial commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="header-body">
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
+    <el-menu :default-active="activeIndex"
+             class="el-menu-demo"
+             mode="horizontal"
+             @select="handleSelect"
+             background-color="#545c64"
+             text-color="#fff"
+             active-text-color="#ffd04b">
       <el-menu-item index="1">FJUT 515创新实验室</el-menu-item>
       <el-menu-item index="2"></el-menu-item>
       <el-menu-item index="3"></el-menu-item>
