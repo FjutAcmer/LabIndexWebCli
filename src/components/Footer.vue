4c5b459f73348911b8a8c0704bb2a86a92113aed
axiang <wyxworkmail@163.com>
2020-03-16 13:45:32 +0800

    fix():添加备案跳转

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer-body">
-    <span class="backup-span">闽ICP备19024997号</span>
+    <span class="backup-span" @click="toBeian()">闽ICP备19024997号</span>
     <br />
     <span class="power-span">Power by ©福建工程学院-信息科学与工程学院-515创新实验室</span>
   </div>
@@ -8,7 +8,11 @@
 
 <script>
 export default {
-
+  methods: {
+    toBeian () {
+      window.open('http://www.beian.miit.gov.cn/')
+    }
+  }
 }
 </script>
 
@@ -20,6 +24,7 @@ export default {
 }
 
 .backup-span {
+  cursor: pointer;
   color: white;
   line-height: 30px;
 }
